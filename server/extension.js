b0396be82b7de31a5f52568feeaa42fe48aaa44d
Shinta <shinta.liem@gmail.com>
2023-10-27 11:20:20 +0800

    Added hooks and message struct

--- server/extension.js
+++ server/extension.js
@@ -1,7 +1,13 @@
 // extension.ts
+var extension_hooks = {
+  "pre_request_execute": function(ctx, api, request) {
+    ctx.app.debug("pre_request_execute", api, JSON.stringify(request, null, 2));
+  }
+};
 var extension_default = {
+  hooks: extension_hooks,
   init: (ctx) => {
-    ctx.app.info("Omni Extension Template loaded");
+    ctx.app.info("Omni Debugger Extension loaded");
   }
 };
 export {
--- src/server/extension.ts
+++ src/server/extension.ts
@@ -1,5 +1,24 @@
+import { IMessage } from 'omni-shared'
+
+interface IOmniSSEMessageDebuggerLog extends IMessage {
+  type: 'omni-extension-debugger:log';
+  body: {
+    message: string;
+    options: {
+      type?: 'info' | 'success' | 'warning' | 'error';
+    }
+  }
+}
+
+const extension_hooks = {
+  'pre_request_execute': function(ctx: any, api: string, request: any) {
+    ctx.app.debug('pre_request_execute', api, JSON.stringify(request, null, 2))
+  }
+}
+
 export default {
+  hooks: extension_hooks,
   init: (ctx: any) => {
-    ctx.app.info('Omni Extension Template loaded')
+    ctx.app.info('Omni Debugger Extension loaded')
   }
 }
