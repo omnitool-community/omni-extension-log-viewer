dcda2fb75388ab86524ddb6d96ee5a795e216ae5
Shinta <shinta.liem@gmail.com>
2023-11-16 14:48:37 +0800

    Truncate string > 300 char long

--- server/extension.js
+++ server/extension.js
@@ -1,44 +1,66 @@
 // extension.ts
 import { OmniSSEMessages } from "omni-shared";
 var EXTENSION_ID = "omni-extension-log-viewer";
+var LOG_EXPIRE_IN_MS = 1e3 * 60 * 60 * 2;
+var MAX_LENGTH = 300;
+function saveLog(ctx, log) {
+  if (ctx.app.kvStorage) {
+    ctx.app.kvStorage.set(`log:${log.sessionId}.${log.timestamp}`, log, Date.now() + LOG_EXPIRE_IN_MS);
+  }
+}
+async function sendLogMessage(ctx, log) {
+  const messagingService = ctx.app.services.get("messaging");
+  await messagingService.send(log.sessionId, {
+    type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
+    body: {
+      extensionId: EXTENSION_ID,
+      eventId: "log",
+      eventArgs: log
+    }
+  });
+}
+function truncateJSON(json) {
+  let truncated = Array.isArray(json) ? [] : {};
+  for (const key in json) {
+    if (Object.prototype.hasOwnProperty.call(json, key)) {
+      if (typeof json[key] === "object") {
+        truncated[key] = truncateJSON(json[key]);
+      } else if (typeof json[key] === "string") {
+        truncated[key] = json[key].length > MAX_LENGTH ? json[key].substring(0, MAX_LENGTH) + "..." : json[key];
+      } else {
+        truncated[key] = json[key];
+      }
+    }
+  }
+  return truncated;
+}
 var extension_hooks = {
   "pre_request_execute": async function(ctx, exec_ctx, api, details) {
     if (exec_ctx.sessionId) {
-      const message = {
-        type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
-        body: {
-          extensionId: EXTENSION_ID,
-          eventId: "log",
-          eventArgs: {
-            timestamp: (/* @__PURE__ */ new Date()).getTime(),
-            type: "info" /* INFO */,
-            message: `Executing ${api}`,
-            details
-          }
-        }
+      const log = {
+        sessionId: exec_ctx.sessionId,
+        timestamp: (/* @__PURE__ */ new Date()).getTime(),
+        jobId: exec_ctx.jobId,
+        type: "info" /* INFO */,
+        message: `Executing ${api}`,
+        details: truncateJSON(details)
       };
-      const messagingService = ctx.app.services.get("messaging");
-      await messagingService.send(exec_ctx.sessionId, message);
+      saveLog(ctx, log);
+      sendLogMessage(ctx, log);
     }
   },
   "post_request_execute": async function(ctx, exec_ctx, api, details) {
     if (exec_ctx.sessionId) {
-      const logType = details.result?.error ? "error" /* ERROR */ : "info" /* INFO */;
-      const message = {
-        type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
-        body: {
-          extensionId: EXTENSION_ID,
-          eventId: "log",
-          eventArgs: {
-            timestamp: (/* @__PURE__ */ new Date()).getTime(),
-            type: logType,
-            message: `Received result from ${api}`,
-            details
-          }
-        }
+      const log = {
+        sessionId: exec_ctx.sessionId,
+        timestamp: (/* @__PURE__ */ new Date()).getTime(),
+        jobId: exec_ctx.jobId,
+        type: details.result?.error ? "error" /* ERROR */ : "info" /* INFO */,
+        message: `Result received from ${api}`,
+        details: truncateJSON(details)
       };
-      const messagingService = ctx.app.services.get("messaging");
-      await messagingService.send(exec_ctx.sessionId, message);
+      saveLog(ctx, log);
+      sendLogMessage(ctx, log);
     }
   }
 };
--- src/server/extension.ts
+++ src/server/extension.ts
@@ -1,3 +1,4 @@
+import { truncate } from 'fs';
 import { OmniSSEMessages } from 'omni-shared'
 
 const EXTENSION_ID = 'omni-extension-log-viewer'
@@ -8,53 +9,82 @@ enum LogType {
   ERROR = 'error',
 }
 
+interface ILog {
+  sessionId: string;
+  timestamp: number;
+  jobId?: string;
+  type: LogType;
+  message: string;
+  details: any;
+}
+
+const LOG_EXPIRE_IN_MS = 1000 * 60 * 60 * 2 // 2 hours
+const MAX_LENGTH = 300
+
+function saveLog(ctx: any, log: ILog) {
+  if (ctx.app.kvStorage) {
+    ctx.app.kvStorage.set(`log:${log.sessionId}.${log.timestamp}`, log, Date.now() + LOG_EXPIRE_IN_MS)
+  }
+}
+
+async function sendLogMessage(ctx: any, log: ILog) {
+  const messagingService = ctx.app.services.get('messaging')
+  await messagingService.send(log.sessionId, {
+    type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
+    body: {
+      extensionId: EXTENSION_ID,
+      eventId: 'log',
+      eventArgs: log
+    }
+  })
+}
+
+function truncateJSON(json: any) {
+  let truncated: any = Array.isArray(json) ? [] : {}; // Initialize as array or object based on input
+
+  for (const key in json) {
+    if (Object.prototype.hasOwnProperty.call(json, key)) {
+      if (typeof json[key] === 'object') {
+        truncated[key] = truncateJSON(json[key]); // Recursively truncate
+      } else if (typeof json[key] === 'string') {
+        truncated[key] = json[key].length > MAX_LENGTH ? json[key].substring(0, MAX_LENGTH) + '...' : json[key];
+      } else {
+        truncated[key] = json[key]; // Copy other types as is
+      }
+    }
+  }
+
+  return truncated;
+}
+
 const extension_hooks = {
   'pre_request_execute': async function(ctx: any, exec_ctx: any, api: string, details: any) {
     if (exec_ctx.sessionId) {
-      const message = {
-        type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
-        body: {
-          extensionId: EXTENSION_ID,
-          eventId: 'log',
-          eventArgs: {
-            timestamp: new Date().getTime(),
-            type: LogType.INFO,
-            message: `Executing ${api}`,
-            details,
-          }
-        }
+      const log: ILog = {
+        sessionId: exec_ctx.sessionId,
+        timestamp: new Date().getTime(),
+        jobId: exec_ctx.jobId,
+        type: LogType.INFO,
+        message: `Executing ${api}`,
+        details: truncateJSON(details),
       }
-      const messagingService = ctx.app.services.get('messaging')
-      await messagingService.send(exec_ctx.sessionId, message)
+
+      saveLog(ctx, log)
+      sendLogMessage(ctx, log)
     }
   },
   'post_request_execute': async function(ctx: any, exec_ctx: any, api: string, details: any) {
     if (exec_ctx.sessionId) {
-      const logType = details.result?.error ? LogType.ERROR : LogType.INFO
-      const message = {
-        type: OmniSSEMessages.CUSTOM_EXTENSION_EVENT,
-        body: {
-          extensionId: EXTENSION_ID,
-          eventId: 'log',
-          eventArgs: {
-            timestamp: new Date().getTime(),
-            type: logType,
-            message: `Received result from ${api}`,
-            details,
-          }
-        }
+      const log: ILog = {
+        sessionId: exec_ctx.sessionId,
+        timestamp: new Date().getTime(),
+        jobId: exec_ctx.jobId,
+        type: details.result?.error ? LogType.ERROR : LogType.INFO,
+        message: `Result received from ${api}`,
+        details: truncateJSON(details),
       }
-      const messagingService = ctx.app.services.get('messaging')
-      // if (logType === LogType.ERROR) {
-      //   await messagingService.send(exec_ctx.sessionId, {
-      //     type: OmniSSEMessages.SHOW_EXTENSION,
-      //     body: {
-      //       extensionId: EXTENSION_ID,
-      //       page: 'index.html'
-      //     }
-      //   })
-      // }
-      await messagingService.send(exec_ctx.sessionId, message)
+      saveLog(ctx, log)
+      sendLogMessage(ctx, log)
     }
   }
 }
